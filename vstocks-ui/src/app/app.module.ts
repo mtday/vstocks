55047aee058c01c353a629f2ed908f721f730d86
mday <mday@eitccorp.com>
2020-08-06 20:29:06 -0500

    created initial ng components

--- vstocks-ui/src/app/app.module.ts
+++ vstocks-ui/src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { FixedpluginComponent } from './shared/fixedplugin/fixedplugin.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { IconsComponent } from './pages/icons/icons.component';
+import { MapsComponent } from './pages/maps/maps.component';
+import { NotificationsComponent } from './pages/notifications/notifications.component';
+import { TableComponent } from './pages/table/table.component';
+import { TypographyComponent } from './pages/typography/typography.component';
+import { UpgradeComponent } from './pages/upgrade/upgrade.component';
+import { UserComponent } from './pages/user/user.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    FooterComponent,
+    FixedpluginComponent,
+    DashboardComponent,
+    IconsComponent,
+    MapsComponent,
+    NotificationsComponent,
+    TableComponent,
+    TypographyComponent,
+    UpgradeComponent,
+    UserComponent,
+    AdminLayoutComponent
   ],
   imports: [
     BrowserModule,
